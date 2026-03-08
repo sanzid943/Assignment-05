faea401217a62bc8087cdd0d613011e6ce8b26f5
sanzid943 <sanzidmostofa@gmail.com>
2026-03-09 01:19:34 +0600

    added button toggle

--- login.js
+++ login.js
@@ -73,3 +73,76 @@ const loadIssues = () => {
         }); 
 };
 
+const displayIssues=(issues)=>{
+    const cardContainer =document.getElementById('card-container');
+    const issueCount =document.getElementById('issue-count');
+    cardContainer.innerHTML='';
+
+    for(let issue of issues){
+        const cardDiv=document.createElement('div');
+        cardDiv.innerHTML=`
+         <div onclick="loadModal(${issue.id})" class="card bg-white rounded-xl p-5 space-y-3 h-full border-t-4 ${issue.status==='open' ? 'border-green-500' : 'border-purple-500'}">
+                <div class="flex justify-between">
+                    <img src="${issue.status=='open' ? './B13-A5-Github-Issue-Tracker/assets/Open-Status.png' : './B13-A5-Github-Issue-Tracker/assets/Closed- Status .png'}" alt="">
+                    <p class="px-2 py-1 rounded text-white text-xs ${
+                        issue.priority === 'high' 
+                        ? 'bg-red-500' 
+                        : issue.priority === 'medium' 
+                        ? 'bg-yellow-500 text-black' 
+                        : 'bg-gray-500'
+                    }">
+                        ${issue.priority}
+                    </p>
+                </div>
+                <div>
+                    <h2 class="text-[18px] font-bold">${issue.title}</h2>
+                    <p class="text-[12px]">${issue.description}</p>
+                </div>
+                <div>${createElements(issue.labels)}</div><hr>
+                <div>
+                    <h2 class="font-bold text-[14px]">#${issue.id} by ${issue.author}</h2>
+                    <p class="text-[12px]">${issue.createdAt.split('T')[0]}</p>
+                </div>
+            </div>
+        `
+        cardContainer.append(cardDiv);
+        
+        issueCount.innerText=cardContainer.children.length;
+    }
+    manageSpinner(false);
+}
+
+loadIssues();
+
+const allBtn = document.getElementById("all-btn");
+const openBtn = document.getElementById("open-btn");
+const closedBtn = document.getElementById("closed-btn");
+
+const buttons =[allBtn,openBtn,closedBtn];
+
+const activeButton=(active)=>{
+    buttons.forEach(btn=>{
+        if(btn===active){
+            btn.classList.add('btn-primary');
+        }else{
+            btn.classList.remove('btn-primary');
+        }
+    })
+}
+
+allBtn.addEventListener("click", () => {
+    displayIssues(allIssues);
+    activeButton(allBtn);
+});
+
+openBtn.addEventListener("click", () => {
+    const openIssues = allIssues.filter(issue => issue.status === "open");
+    displayIssues(openIssues);
+    activeButton(openBtn);
+});
+
+closedBtn.addEventListener("click", () => {
+    const closedIssues = allIssues.filter(issue => issue.status === "closed");
+    displayIssues(closedIssues);
+    activeButton(closedBtn);
+});
